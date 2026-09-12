7af0258e7e67aa42218d1370c826f7f3562d933d
Sophie Student <sophie.student16@gmail.com>
2026-09-12 21:09:47 +0000

    First commit of basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Created By: Sophie Student
+# Date: Sep. 12th, 2026
+# This program displays a series of math equations.
+def main():
+    print("2 + 3 = {}".format(2 + 3))  # addition
+    print("6 - 3 = {}".format(6 - 3))  # subtraction
+    print("8 * 2 = {}".format(8 * 2))  # multiplication
+    print("7 / 2 = {}".format(7 / 2))  # division
+    print("2 ** 3 = {}".format(2**3))  # exponent
+    print("16 ** 0.5 = {}".format(16**0.5))  # square root
+    print("1+3*4 = {}".format(1 + 3 * 4))  # bedmas
+    print("10 / 5 = {}".format(10 / 5))  # division
+    print("10 % 7 = {}".format(10 % 7))  # modulo (remainder)
+
+
+if __name__ == "__main__":
+    main()
